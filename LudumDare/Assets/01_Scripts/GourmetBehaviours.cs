180b0a498eea47ec176a8c0d0d7b3f9175f7e244
natwPine <natw.mezza@hotmail.fr>
2022-04-03 09:40:43 +0200

    NMezza - [Feature]indigestion

--- LudumDare/Assets/01_Scripts/GourmetBehaviours.cs
+++ LudumDare/Assets/01_Scripts/GourmetBehaviours.cs
@@ -8,10 +8,11 @@ public class GourmetBehaviours : MonoBehaviour
     public static GourmetBehaviours instance;
 
     [Header("Param")]
+    public float m_MaxStarvingPoint;
     public float m_StarvingPoint;
     public float m_ReduceStarvingPoint;
 
-    public float score;
+    public bool m_IsIndigestion = false;
 
     public TMPro.TMP_Text scoreText;
 
@@ -19,6 +20,8 @@ public class GourmetBehaviours : MonoBehaviour
     [Min(0.2f)]
     public float m_StravingTime;
     private Timer t_StravingTimer;
+    public float m_IndigestionTime;
+    private Timer t_IndegestionTimer;
 
     void Awake()
     {
@@ -31,6 +34,7 @@ public class GourmetBehaviours : MonoBehaviour
     private void Start()
     {
         t_StravingTimer = new Timer(m_StravingTime, Starving);
+        t_IndegestionTimer = new Timer(m_IndigestionTime, StopIndegestion);
 
         t_StravingTimer.ResetPlay();
     }
@@ -45,20 +49,46 @@ public class GourmetBehaviours : MonoBehaviour
         t_StravingTimer.ResetPlay();
     }
 
+    public void IncreaseStarvingPoint(float point)
+    {
+        m_StarvingPoint += point;
+
+        if (IsIndigestion())
+        {
+            m_StarvingPoint = m_MaxStarvingPoint;
+            m_IsIndigestion = true;
+            t_IndegestionTimer.ResetPlay();
+        }
+
+        t_StravingTimer.ResetPlay();
+    }
+
+    public bool IsIndigestion()
+    {
+        return m_StarvingPoint > m_MaxStarvingPoint;
+    }
+
+    public void StopIndegestion()
+    {
+        m_IsIndigestion = false;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         Food_Behaviours food;
 
-        if (!collision.gameObject.TryGetComponent<Food_Behaviours>(out food))
+        if (!collision.gameObject.TryGetComponent<Food_Behaviours>(out food) || m_IsIndigestion)
             return;
 
-        score += food.param.GetPoint(food.m_Level);
+        IncreaseStarvingPoint(food.param.GetPoint(food.m_Level));
 
         UpdateScoreText();
     }
 
+
+
     private void UpdateScoreText()
     {
-        scoreText.text = score.ToString();
+        scoreText.text = m_StarvingPoint.ToString();
     }
 }
